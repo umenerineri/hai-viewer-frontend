d02a9562c4ce46864c8fe9879a17ee84bc7f3ba0
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-11-06 23:18:37 +0900

    refactor ok

--- src/page/ViewerPage/hooks.ts
+++ src/page/ViewerPage/hooks.ts
@@ -1,9 +1,8 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo } from "react";
 
 import { ViewerDrawingData } from "@/model";
 
 import { useViewerData } from "@/usecase/getViwerData";
-import { AbsoluteVector, getAbsoluteVectorToShiftForCentering } from "@/utils/viewer";
 
 export const useViewer = () => {
     const { data, refetch } = useViewerData();
@@ -33,28 +32,4 @@ export const useViewer = () => {
         grid: { rows, cols },
         offset: { x: offsetX, y: offsetY },
     };
-};
-
-export const useViewerTransform = () => {
-    const [absoluteVectorToShiftForCentering, setAbsoluteVectorToShiftForCentering] =
-        useState<AbsoluteVector>({ height: 0, width: 0 });
-
-    const handleReset = useCallback(
-        (dataLength: number) => {
-            const absoluteVector: AbsoluteVector = getAbsoluteVectorToShiftForCentering(
-                dataLength,
-                window.innerHeight
-            );
-            console.log(absoluteVector);
-
-            setAbsoluteVectorToShiftForCentering({
-                height: absoluteVector.height,
-                width: absoluteVector.width,
-            });
-            console.log(absoluteVector);
-        },
-        [setAbsoluteVectorToShiftForCentering]
-    );
-
-    return { absoluteVectorToShiftForCentering, handleReset };
 };
